37abc514a7114a7b8a5da8792249d60261508564
TranDanh1122 <trandanh14042000@gmail.com>
2025-02-06 16:40:39 +0700

    responsive category detail

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -2,22 +2,22 @@ import React from "react";
 import { Button } from "./ui/button";
 const ProductItem = React.memo(({ type, isNew }: { type?: string, isNew: boolean }) => {
     return <>
-        <div className={`flex items-center justify-between ${type == "reverse" ? "flex-row-reverse" : "flex-row"}`}>
-            <picture className="w-5/12">
+        <div className={`flex items-center flex-col gap-8 md:gap-0 lg:flex-row justify-between ${type == "reverse" ? "lg:flex-row-reverse" : "flex-row"}`}>
+            <picture className="lg:w-5/12 w-full">
                 <source media="(min-width: 1024px)" srcSet="/assets/shared/desktop/image-xx99-mark-two-headphones.jpg" />
-                <source media="(min-width: 768px)" srcSet="/assets/shared/tablet/image-xx99-mark-two-headphones.jpg" />
-                <img src="/assets/shared/mobile/image-xx99-mark-two-headphones.jpg" alt="Speaker ZX9" className="w-full h-full object-cover" />
+                <source media="(min-width: 768px)" srcSet="/assets/shared/mobile/image-xx99-mark-two-headphones.jpg" />
+                <img src="/assets/shared/mobile/image-xx99-mark-two-headphones.jpg" alt="Speaker ZX9" className="w-full h-full object-cover lg:aspect-square md:aspect-video" />
             </picture>
-            <div className="w-5/12">
-                {isNew && <span className="over  lg:text-left text-center text-d8">
+            <div className="lg:w-5/12 w-full">
+                {isNew && <span className="over block lg:text-left text-center text-d8">
                     NEW PRODUCT
                 </span>}
-                <h2 className="h2 mt-4 mb-8"> XX99 Mark II Headphones</h2>
-                <p className="text-black text-opacity-50 body font-medium">
+                <h2 className="h2 mt-4 mb-8 w-1/2 lg:w-full text-center mx-auto"> XX99 Mark II Headphones</h2>
+                <p className="text-black text-opacity-50 body font-medium w-4/5 lg:w-full text-center mx-auto">
                     The new XX99 Mark II headphones is the pinnacle of pristine audio.
                     It redefines your premium headphone experience by reproducing the balanced depth and precision of studio-quality sound.
                 </p>
-                <Button className=" w-fit bg-d8 hover:bg-fb py-6 px-8 mt-10 rounded-none subtitle lg:mx-0 mx-auto">
+                <Button className=" w-fit bg-d8 hover:bg-fb py-6 px-8 mt-10 rounded-none subtitle lg:mx-0 mx-auto flex">
                     See Product
                 </Button>
             </div>
